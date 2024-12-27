bf5b865550856f19fbdfc341c81f3f86310dd675
JuliePlagias <julieplagias@gmail.com>
2024-12-27 12:55:04 +0100

    Feat - brief ex1

---
+++ 01-classes-and-objects/voiture.js
@@ -0,0 +1,25 @@
+class Voiture{
+    make;
+    model;
+    year;
+    color;
+
+    constructor(make, model, year, color)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    showDetails()
+    {
+        let str = `The car is a ${this.color} ${this.make} ${this.model} from ${this.year}`;
+        console.log(str);
+    }
+}
+
+const car = new Voiture("Corvette", "Z06", 2019, "red");
+const car2 = new Voiture("Ford", "Raptor", 2000, "red");
+car.showDetails();
+car2.showDetails();
